940f6860fbd09ad801cf6597dd5a4b66a7b27bac
ayshubham <71246748+ayshubham@users.noreply.github.com>
2021-10-13 02:09:59 +0530

    form

--- registration/registration.js
+++ registration/registration.js
@@ -6,4 +6,23 @@ function next1(e) {
   //   document.getElementById("btn1").addEventListener("click", (e) => {});
 }
 
-next1();
+function next2(e) {
+  console.log(e);
+  e.preventDefault();
+  document.getElementsByClassName("secondary")[0].style.transform =
+    "translateX(-200%)";
+  //   document.getElementById("btn1").addEventListener("click", (e) => {});
+}
+
+function next2Back(e) {
+  console.log(e);
+  e.preventDefault();
+  document.getElementsByClassName("secondary")[0].style.transform =
+    "translateX(0%)";
+  //   document.getElementById("btn1").addEventListener("click", (e) => {});
+}
+
+function register(e) {
+  e.preventDefault();
+  console.log(e);
+}
